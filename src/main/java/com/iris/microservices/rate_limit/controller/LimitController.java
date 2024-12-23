c91f898a432bd9fe53dbe2e02f41545df9daa659
shashank189612 <shashanknm18@gmail.com>
2024-12-23 21:19:19 +0530

    fetched the configurations from the properties file by creating configuration  class

--- src/main/java/com/iris/microservices/rate_limit/controller/LimitController.java
+++ src/main/java/com/iris/microservices/rate_limit/controller/LimitController.java
@@ -2,15 +2,20 @@ package com.iris.microservices.rate_limit.controller;
 
 
 import com.iris.microservices.rate_limit.bean.Limit;
+import com.iris.microservices.rate_limit.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitController {
 
+    @Autowired
+    private Configuration configuration;
+
 
     @GetMapping("/limits")
     public Limit getLimits(){
-        return new Limit(1,999);
+        return new Limit(configuration.getMinimum(),configuration.getMaximum());
     }
 }
